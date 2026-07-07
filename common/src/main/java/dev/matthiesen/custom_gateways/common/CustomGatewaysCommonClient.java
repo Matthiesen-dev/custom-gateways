21d7adb6a774e615bcdd2d596627c8c79d93b60a
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-07 15:11:47 -0700

    Add block outline rendering for Portal Frame and implement base position retrieval

--- common/src/main/java/dev/matthiesen/custom_gateways/common/CustomGatewaysCommonClient.java
+++ common/src/main/java/dev/matthiesen/custom_gateways/common/CustomGatewaysCommonClient.java
@@ -1,13 +1,26 @@
 package dev.matthiesen.custom_gateways.common;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.matthiesen.common.matthiesen_lib.abstracts.AbstractCommonClientMod;
+import dev.matthiesen.custom_gateways.common.block.PortalFrameBlock;
 import dev.matthiesen.custom_gateways.common.client.renderer.block.PortalFrameBlockRenderer;
 import dev.matthiesen.custom_gateways.common.client.renderer.item.PortalFrameItemRenderer;
 import dev.matthiesen.custom_gateways.common.item.PortalFrameItem;
 import dev.matthiesen.custom_gateways.common.registry.BlockEntityRegistry;
+import dev.matthiesen.custom_gateways.common.registry.BlockRegistry;
 import dev.matthiesen.custom_gateways.common.registry.ItemRegistry;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.client.GeoRenderProvider;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
@@ -31,6 +44,46 @@ public final class CustomGatewaysCommonClient extends AbstractCommonClientMod {
         INSTANCE.registerEntityRenderers(registry ->
                 registry.registerBlockEntityRenderer(BlockEntityRegistry.PORTAL_FRAME_BE.get(), context -> new PortalFrameBlockRenderer())
         );
+
+        INSTANCE.registerBlockOutlineListener(context -> {
+            ClientLevel level = context.level();
+            BlockPos basePos = getBasePos(level, context.blockHitResult().getBlockPos());
+
+            if (basePos == null) return true;
+
+            PoseStack poseStack = context.poseStack();
+            MultiBufferSource bufferSource = context.multiBufferSource();
+            Camera camera = context.camera();
+            VoxelShape shape = level.getBlockState(basePos).getShape(level, basePos);
+
+            double x = basePos.getX() - camera.getPosition().x();
+            double y = basePos.getY() - camera.getPosition().y();
+            double z = basePos.getZ() - camera.getPosition().z();
+
+            LevelRenderer.renderVoxelShape(
+                    poseStack,
+                    bufferSource.getBuffer(RenderType.lines()),
+                    shape,
+                    x, y, z,
+                    0.0F, 0.0F, 0.0F, 0.4F, false
+            );
+
+            return false;
+        });
+    }
+
+    public static @Nullable BlockPos getBasePos(Level level, BlockPos hitPos) {
+        BlockState hitState = level.getBlockState(hitPos);
+
+        if (hitState.is(BlockRegistry.PORTAL_FRAME.get())) {
+            if (hitState.getValue(PortalFrameBlock.IS_SLAVE)) {
+                return hitPos.below();
+            } else {
+                return hitPos;
+            }
+        }
+
+        return null;
     }
 
     private static <T extends PortalFrameItem> GeoRenderProvider makeRendererProvider(GeoItemRenderer<T> renderer) {
